d803c3489cbf1a79ab1f305e0010cfb183fdff79
BrayanRK <98141343+BrayanRK@users.noreply.github.com>
2026-07-19 16:25:55 -0500

    Update fuck.js

--- commands/nsfw/fuck.js
+++ commands/nsfw/fuck.js
@@ -8,16 +8,16 @@ module.exports = {
 
   async execute(sock, jid, msg) {
     const frases = [
-      '{autor} tomo a {objetivo} con tanta hambre que el cuarto entero parecio temblar. 🔥',
-      '{autor} arrastro a {objetivo} a un encuentro salvaje donde nadie penso en frenar. 😈',
-      '{objetivo} quedo completamente dominado por la intensidad con la que {autor} lo devoro. 🖤',
-      '{autor} y {objetivo} se perdieron en una escena tan brutal que sobraron las palabras. 🥵',
-      '{autor} se lanzo sobre {objetivo} con deseos acumulados y cero intenciones de detenerse. 💥',
-      '{objetivo} no tuvo escapatoria cuando {autor} decidio tomar el control del juego. 😏',
-      '{autor} convirtio a {objetivo} en el centro de una noche llena de puro exceso. 🔞',
-      '{autor} hizo suyo a {objetivo} con una energia tan feroz que encendio todo alrededor. 💋',
-      '{autor} dejo claro con {objetivo} que esa noche no iba a ser para nada tranquila. 😮‍💨',
-      '{objetivo} termino atrapado en el ritmo salvaje que {autor} impuso sin piedad. 💣'
+      '{autor} folló a {objetivo} como un animal, sin piedad. 🔥',
+      '{autor} jodió a {objetivo} hasta dejarlo hecho mierda. 😈',
+      '{objetivo} quedó destruido/a cuando {autor} lo/la rompió a pollazos. 🖤',
+      '{autor} penetró a {objetivo} con tanta fuerza que gritó. 🥵',
+      '{autor} se corrió dentro de {objetivo} marcando su territorio. 💥',
+      '{objetivo} no pudo escapar de la verga de {autor}. 😏',
+      '{autor} usó a {objetivo} como su puto/a personal. 🔞',
+      '{autor} dejó a {objetivo} manco/a de tanto follar. 💋',
+      '{autor} violó a {objetivo} con la mirada antes de con la verga. 😮‍💨',
+      '{objetivo} terminó sangrando después de la brutalidad de {autor}. 💣'
     ];
 
     await enviarReactionDelirius(sock, jid, msg, 'fuck', frases);
