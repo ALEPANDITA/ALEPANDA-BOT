746968fd05bf659d185055c729591889dd89a1ae
BrayanRK <98141343+BrayanRK@users.noreply.github.com>
2026-07-19 16:10:28 -0500

    Update anal.js

--- commands/nsfw/anal.js
+++ commands/nsfw/anal.js
@@ -7,12 +7,19 @@ module.exports = {
   groupOnly: true,
 
   async execute(sock, jid, msg) {
-    await enviarReactionDelirius(
-      sock,
-      jid,
-      msg,
-      'anal',
-      (tag) => `🥵 ${tag} fue atrapado en una escena *anal*`
-    );
+    const frases = [
+      '{autor} arrincono a {objetivo} contra la pared y la noche tomo un rumbo demasiado intenso. 🥵',
+      '{autor} no le dio escapatoria a {objetivo} y termino desatando una escena bastante atrevida. 🔥',
+      '{autor} jalo a {objetivo} con firmeza y lo envolvio en un momento subido de tono. 😈',
+      '{autor} tenia otras intenciones con {objetivo}, y vaya que las dejo claritas. 💋',
+      '{objetivo} quedo a merced de {autor}, que venia con ganas de portarse muy mal. 😏',
+      '{autor} sorprendio a {objetivo} con una embestida tan intensa que el chat entero sintio la tension. 💥',
+      '{autor} se adueño del momento con {objetivo} y encendio todo alrededor. 🖤',
+      '{autor} llevo a {objetivo} al limite con una jugada descarada y sin vergüenza. 🥀',
+      '{autor} tomo el control de {objetivo} y convirtio el instante en puro fuego. 🔞',
+      '{autor} termino provocando a {objetivo} de una forma tan salvaje que ya no hubo vuelta atras. 😵‍💫'
+    ];
+
+    await enviarReactionDelirius(sock, jid, msg, 'anal', frases);
   }
 };
