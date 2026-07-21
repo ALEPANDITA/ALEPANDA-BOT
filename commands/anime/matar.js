31b317f3103432152c00c03d88edb4cd062815f3
ALEPANDITA <jpaezpinedo@gmail.com>
2026-07-20 22:07:22 -0700

    Reemplazar comando matar para usar Delirius API en vez de nekos.best/waifu.pics

--- commands/anime/matar.js
+++ commands/anime/matar.js
@@ -1,12 +1,12 @@
-const { enviarReaccion } = require('../../lib/animereact');
+const { enviarReaccionDelirius } = require('../../lib/animereact');
 
 module.exports = {
   name: 'matar',
   category: 'anime',
   description: 'lo mata con un gif de anime (menciona o responde a la persona)',
-  execute: (sock, jid, msg) => enviarReaccion(sock, jid, msg, {
-    tipo: 'shoot',
-    emoji: '🔫',
+  execute: (sock, jid, msg) => enviarReaccionDelirius(sock, jid, msg, {
+    endpoint: 'kill',
+    emoji: '🔪',
     conObjetivo: (yo, el) => `@${yo} mató a @${el}`,
     sinObjetivo: (yo) => `@${yo} está en modo asesino`
   })
